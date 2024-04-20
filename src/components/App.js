2313ae917c0a8b9f84881c0c34cbd08d0e6d0445
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-04-20 22:14:19 +0530

    App modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
+import React from 'react';
+import '../styles/App.css';
+import ChildComponent1 from './ChildComponent1';
+import ChildComponent2 from './ChildComponent2';
 
-import React from "react";
-import './../styles/App.css';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedOption: ''
+    };
+  }
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+  handleOptionChange = (option) => {
+    this.setState({ selectedOption: option });
+  }
+
+  render() {
+    return (
+      <div className="parent">
+        <h1>Parent Component</h1>
+        <div className="child1">
+            <h2>Child Component 1</h2>
+            <ChildComponent1 onOptionChange={this.handleOptionChange} />
+        </div>
+        <div  className="child2">
+            <h2>Child Component 2</h2>
+            <ChildComponent2 onOptionChange={this.handleOptionChange} />
+        </div>
+        <p>Selected Option: {this.state.selectedOption}</p>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
+
